0db938c242fb0faa557c4e01ebadaac628480f68
zlicdt <zhuhaoming135@outlook.com>
2026-01-03 10:48:41 +0800

    feat: Add help/version argument

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 */
 use anyhow::Result;
 use chrono::Local;
+use clap::Command;
 mod render;
 mod syspf;
 mod utils;
@@ -16,6 +17,15 @@ mod ioreg;
 mod smc;
 
 fn main() -> Result<()> {
+    /*
+     * For argument, display help & version
+     * clap will auto-provide -h/--help and -V/--version
+     */
+    let _matches = Command::new("apple-smi")
+        .about("Apple Silicon System Management Interface")
+        .version(env!("CARGO_PKG_VERSION"))
+        .get_matches();
+
     // Local time
     println!("{}", Local::now().format("%a %b %e %T %Y"));
     render::render()
